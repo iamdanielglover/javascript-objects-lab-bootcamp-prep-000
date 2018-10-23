aa74b34a0a471293faddec94ce90473a9230078e
Daniel Glover <iamdanielglover@gmail.com>
2018-10-23 17:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj = delete object.key;
+  var obj = Object.assign({}, object);
+  delete obj.key;
 }
