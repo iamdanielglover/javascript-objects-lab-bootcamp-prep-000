b8f294042ac150ad95c7e3bf001248aa635b11a8
Daniel Glover <iamdanielglover@gmail.com>
2018-10-23 16:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, object,{[key]:value});
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  newobj = Object.assign({}, object,{[key]:value});
-  return newobj;
-}
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key]=value;
   return object;
