66f8f4c66362db6be974f0c6c2e4d6e3b13a6fc0
Daniel Glover <iamdanielglover@gmail.com>
2018-10-23 16:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
+var recipes = {};
 
-
-var recipes = {prop: 1};
-
-function updateObjectWithKeyAndValue(object,key,value){
-  var newObj = Object.assign({},object);
-  newObj[key]=value;
-  console.log(newObj);
-  console.log(recipes);
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
 
- updateObjectWithKeyAndValue(recipes,'prop2',2);
-
- function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-   return Object.assign(object,object[key]=value);
- }
-
-destructivelyUpdateObjectWithKeyAndValue(recipes,'prop2',2);
-
- function deleteFromObjectByKey(object, key){
-   var newObj = Object.assign({},object);
-delete newObj[key];
-   return newObj;
-}
-
-deleteFromObjectByKey(recipes,'prop');
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key];
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
   return object;
 }
 
-destructivelyDeleteFromObjectByKey(recipes,'prop');
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  
+  return object;
+}
